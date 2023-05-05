d3179684db3642f880ca2692b7b768be713c65dd
MikeDePacina <mikedpacina@gmail.com>
2023-05-05 10:25:06 -0700

    changed favorite buttons to bookmark, and put my books and favorite list in dropdown under my account

--- bookEx/settings.py
+++ bookEx/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fontawesomefree',
 
 ]
 LOGIN_URL = 'login'  # or actual url
